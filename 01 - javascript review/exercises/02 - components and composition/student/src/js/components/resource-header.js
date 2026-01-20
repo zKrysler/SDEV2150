6ee15d924d5cc898da797d6e3bd500f08b436c9d
Zachary Krysler <zacharykrysler@gmail.com>
2026-01-20 14:46:12 -0700

    added components and moved html to template

---
+++ 01 - javascript review/exercises/02 - components and composition/student/src/js/components/resource-header.js
@@ -0,0 +1,13 @@
+const template =document.createElement('template');
+template.innerHTML= `
+  <header class="mb-4">
+    <div class="d-flex flex-wrap justify-content-between align-items-end gap-2">
+      <div>
+        <h1 class="h3 mb-1">NAIT Resource Directory</h1>
+        <p class="text-body-secondary mb-0">
+          Find student support services, labs, and campus resources.
+        </p>
+      </div>
+    </div>
+  </header>
+`
