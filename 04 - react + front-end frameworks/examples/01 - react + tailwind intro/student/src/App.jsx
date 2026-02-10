16a48aaf0f7faedcfb69e5bebf92a69b0b18a6e6
Zachary Krysler <zacharykrysler@gmail.com>
2026-02-10 14:49:07 -0700

    added new components and changed header styling

--- 04 - react + front-end frameworks/examples/01 - react + tailwind intro/student/src/App.jsx
+++ 04 - react + front-end frameworks/examples/01 - react + tailwind intro/student/src/App.jsx
@@ -15,8 +15,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> 
+      <h1 className='bg-sky-500'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
