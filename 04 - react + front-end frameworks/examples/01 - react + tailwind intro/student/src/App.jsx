1910823cda449e70597b0572f5892a6c3bafa368
Zachary Krysler <zacharykrysler@gmail.com>
2026-02-11 16:16:17 -0700

    added code from code along

--- 04 - react + front-end frameworks/examples/01 - react + tailwind intro/student/src/App.jsx
+++ 04 - react + front-end frameworks/examples/01 - react + tailwind intro/student/src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
+// import the Header component I just made
 import Header from './components/Header';
+import Filters from './components/Filters'; // don't *need* file extension
+import Results from './components/Results';
+import Details from './components/Details';
 
 function App() {
-  const [count, setCount] = useState(0);
+  return(
+    // I need to wrap everything in a fragment <> </> *if* I have multiple top-level nodes/components —
+    //   e.g. this Header & div component.
+    // DOCS: https://react.dev/reference/react/Fragment
 
-  return <Header />;
+    // If it were just one parent component and a bunch of stuff nested under it, I don't need that.
+    // This lets us avoid nesting everything under plain divs and complicating our DOM tree.
+    <>
+      <Header tagline='Test tagline inserted here, yipee!' />
+      {/* Within a JSX block, comments look like this. I'm using Tailwind classes in this div for auto-spacing! */}
+      <div className="flex flex-col gap-6 lg:grid lg:grid-cols-3 lg:items-stretch">
+        <div className="w-full">
+          <Filters />
+        </div>
+        <div className="w-full">
+          <Results />
+        </div>
+        <div className="w-full">
+          <Details />
+        </div>
+      </div>
+    </>
+
+  );
 }
 
 export default App;
--- 04 - react + front-end frameworks/examples/01 - react + tailwind intro/student/src/components/Header.jsx
+++ 04 - react + front-end frameworks/examples/01 - react + tailwind intro/student/src/components/Header.jsx
@@ -1,11 +1,18 @@
-export default function Header() {
+// two ways to ingest props as a functional componenet:
+// 1. function takes one argument -> all props packed within an object
+//  eg: functionBio(person) {return <p>{ person.name } is { person.age } years old.</p>}
+
+// 2. function takes destructured arguments - . only extracts what you need
+//  eg: function Bio({ name, age }) {return <p>{ name } is { age } years old. </p>} 
+
+export default function Header({ tagline }) {
   return (
     <header className="mb-4">
       <div className="flex flex-wrap items-end justify-between gap-2">
         <div>
           <h1 className="text-xl font-semibold text-sky-600">NAIT Resource Directory</h1>
           <p className="text-sm text-gray-500">
-            Find student support services, labs, and campus resources.
+            { tagline || 'Find student support services, labs, and campus resources.'}
           </p>
         </div>
       </div>
