8fa406752ee8ddb4cc0d4b0492ed88b0ca361565
oaao <oliver+github@abolish.golf>
2026-04-08 16:20:20 -0600

    add [09 - data fetching]: ex - API functions

--- 09 - data fetching/example - data fetching/instructor/frontend/src/api/resources.js
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/api/resources.js
@@ -1,5 +1,21 @@
-const BASE_URL = ""
+const API_BASE_URL = 'http://localhost:3000';
 
-export async function getResources() {}
+export async function getResources() {
+  const res = await fetch(`${API_BASE_URL}/resources`);
 
-export async function getResourceById() {}
+  if (!res.ok) {
+    throw new Error(`Could not load resources: ${res.status}`);
+  }
+
+  return res.json();
+}
+
+export async function getResourceById(resourceId) {
+  const res = await fetch(`${API_BASE_URL}/resources/${resourceId}`);
+
+  if (!res.ok) {
+    throw new Error(`Could not load resource: ${res.status}`);
+  }
+
+  return res.json();
+}
